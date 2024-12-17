5cc4aac9a298499691c392389a06dc310b48c2c9
MF366 <real_mf366@yahoo.com>
2024-12-17 18:06:57 +0000

    fixed the context guys

--- core/basecore/screen/context.py
+++ core/basecore/screen/context.py
@@ -3,18 +3,21 @@ import keyboard
 
 
 class Context:
-    def __init__(self, value: str, events: dict[keyboard._Key, list[str]], keyword_formatters: dict[str, Any] | Any = None, title: str | None = None) -> None:
+    def __init__(self, screen, value: str, keyword_formatters: dict[str, Any] | Any = None, title: str | None = None) -> None:
         self._VALUE: str = value
-        self._EVENTS: dict[int | str, list[str]] = events
         self._TITLE = title
-                
+        self._BASESCREEN = screen
+        
         if not keyword_formatters:
             keyword_formatters = {}
         
         self._FORMATTERS: dict[str, Any] = keyword_formatters
     
-    def handle_value(self, id: str, value: Any):
-        return id, value
+    def handle_input(self, value: str):
+        return value
+    
+    def handle_keypress(self, key: keyboard._Key):
+        return f"Key {key} pressed."
     
     def format(self) -> str:
         if self._VALUE.count('{') == 0 or self._VALUE.count('}') == 0:
@@ -29,6 +32,8 @@ class Context:
         else:
             self._VALUE = char.join(new_value)
             
+        self._BASESCREEN.read_input()
+            
     def run_operation(self, operation: str, *args, **kwargs):
         return eval(f"{self}._VALUE.{operation}(*{args}, **{kwargs})")
     
@@ -39,10 +44,6 @@ class Context:
     @title.setter
     def title(self, value: str) -> None:
         self._TITLE: str = value
-        
-    @property
-    def events(self) -> dict[int | str, list[str]]:
-        return self._EVENTS
 
     def __str__(self) -> str:
         return self._VALUE # [i] no formatting is you do __str__
