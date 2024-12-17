3391aff346de75a4c76423c3ecab78c3e47c1f3c
MF366 <real_mf366@yahoo.com>
2024-12-17 19:04:38 +0000

    oof, gonna do more next day

--- core/basecore/__init__.py
+++ core/basecore/__init__.py
@@ -8,30 +8,46 @@ filebrowser_formatter = formatting.Formatter(True, {})
 
 
 def test_path(path: str, supposed_file: bool = False) -> bool:
+    """
+    ## test_path
+
+    Test the path using 3 different tests.
+
+    :param str path: the path of the directory or file to test
+    :param bool supposed_file: to interpret this path as a file or a directory, defaults to False (interpret as a directory)
+    :return bool: whether the path passed all tests (True) or failed at least one (False)
+    """
+    
     test1 = os.path.exists(path)
     test2 = os.path.isfile(path) if supposed_file else os.path.isdir(path)
     test3 = os.path.exists(os.path.abspath(path))
     
     return all((test1, test2, test3))
-    
 
-class PhotoViewer(context.Context):
-    
 
 class FileBrowser(context.Context):
-    def __init__(self, value, initial_directory: str = '~', keyword_formatters = None, title = None):
-        if initial_directory == '~':
-            self._directory = os.path.expanduser('~')
+    def __init__(self, screen, quick_access_items: tuple[str] | None = None, keyword_formatters = None, title = None):
+        self._SELECTION
         
-        super().__init__(value, keyword_formatters, title)
+        if not quick_access_items:
+            quick_access_items = (os.path.expanduser('~'), os.path.expanduser('~'))
+        
+        self._quick_access_field_values = [f"({i + 1}) {quick_access_items[i] if len(quick_access_items[i]) < 28 else f'{quick_access_items[i][:25]}...'}" for i in range(0, min(len(quick_access_items), 25))]
+        self._quick_access_field = ["╔==========================╗", "║                          ║", "╠====== Quick Access ======╣", "║                          ║"]
+        self._file_browser_field = ["╔==========================╗", "║                          ║", "╠====== File Browser ======╣", "║                          ║"]
+        
+        spam = os.listdir(os.getcwd())
     
-    def handle_value(self, id, value) -> tuple[str, Any]:
-        match id:
-            case 'Please input a path: ':
-                # [i] it must pass all the path tests
-                if not test_path(value):
-                    self._VALUE = "{Back.RED}The path you inserted is invalid!!!{Back.RESET}" + '\n'.join(self._VALUE.split('\n')[1:])
-
-        return super().handle_value(id, value)
-    
-    def handle_hierarchy(self)
+        self._cur_dir_content: list[list[str | int], list[str | int]] = [['..'] + [i if os.path.isdir(i) else 4 for i in spam], [i if os.path.isfile(i) else 4 for i in spam]]
+        
+        for _ in range(self._cur_dir_content[0].count(4)):
+            self._cur_dir_content[0].remove(4)
+            
+        for _ in range(self._cur_dir_content[1].count(4)):
+            self._cur_dir_content[1].remove(4)
+            
+        del spam       
+        
+        value = None
+        
+        super().__init__(screen, value, keyword_formatters, title)
